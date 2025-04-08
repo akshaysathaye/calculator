ac024e63446536e333d4dcc4bd682d97e595be00
akshaysathaye <sathaye05@gmail.com>
2025-04-08 11:10:45 +0530

    Added exponent functionality.

--- calculator.c
+++ calculator.c
@@ -4,6 +4,7 @@ int main(){
   double a, b;
   char operator;
   double answer;
+  int i;
   printf("Enter calculation:\n");
   scanf("%lf%c%lf", &a, &operator, &b);
   switch(operator){
@@ -18,6 +19,12 @@ int main(){
     break;
   case '/':
     answer = a / b;
+    break;
+  case '^':
+    answer = 1;
+    for(i = 0; i < b; i++){
+      answer = answer * a;
+    }
     break;
   default:
     answer = 0;
