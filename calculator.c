4485510920116b359b943b9d28834b7e88291830
akshaysathaye <sathaye05@gmail.com>
2025-04-08 11:26:21 +0530

    added functionality for hex

--- calculator.c
+++ calculator.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
 int main(){
-  double a, b;
+  char d_or_h;
+  int a, b;
   char operator;
-  double answer;
+  int answer;
   int i;
-  printf("Enter calculation:\n");
-  scanf("%lf%c%lf", &a, &operator, &b);
+  printf("Enter d for decimal or h for hexadecimal:\n");
+  scanf("%c", &d_or_h);
+  if(d_or_h == 'd'){
+      printf("Enter calculation:\n");
+      scanf("%d%c%d", &a, &operator, &b);
+  }else if(d_or_h == 'h'){
+    printf("Enter calculation:\n");
+    scanf("%x%c%x", &a, &operator, &b);
+  }else{
+    printf("Invalid input.\n");
+    return 1;
+  }
   switch(operator){
   case '+':
     answer = a + b;
@@ -29,6 +40,9 @@ int main(){
   default:
     answer = 0;
   }
-  printf("The answer is: %lf\n", answer);
+  if(d_or_h == 'd')
+    printf("The answer is: %d\n", answer);
+  else
+    printf("The answer is: %x\n", answer);
   return 0;
 }
